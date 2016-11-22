53cfe86ee0714e0ef6d4eb2dbe3b88116f0c7fc7
jerry <jerry@dynamicminds.co.in>
2016-11-21 21:18:47 -0800

    added code for substraction

--- uptime.sh
+++ uptime.sh
@@ -4,9 +4,9 @@ echo "The system uptime is: "`uptime`
 a=$1
 b=$2
 let c=a+b
+let d=a-b
 <<<<<<< HEAD
-echo "a + b = $c"
-echo "This will result into a conflict"
 =======
 echo "$1 + $2 = $c"
+echo "$1 - $2 = $d"
 >>>>>>> 4812e49ade9e751c9a08db67f95a39ac831768d7
